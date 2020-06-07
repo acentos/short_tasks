5f3ed8bb7d5aae7ece85e5f4c156577bc9eb4923
acentos <artemiy.baranov@gmail.com>
2020-06-07 16:58:28 +0300

    nginx log parser

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Serpstat technical task
 
 Using standard libraries **Python**, collect hints from the site using 1, 2 and 3 letters using **threading** and save the results to a local database **SQLite3** with the ability to save the current state.
+
+## Nginx log parser
+
+Obtaining IP from an **Nginx** access log file, an user agent, a list of links and a short country name. Counting the number of hits. Saving the result to the **csv** file.
---
+++ nginx_log_processor.py
@@ -0,0 +1,107 @@
+import os
+import datetime
+import re
+import subprocess
+import csv
+
+
+NGINX_LOG_DIR = os.path.join(os.getcwd(), 'nginx_log_dir')
+RESULT_FILE = os.path.join(os.getcwd(), 'nginx_log.csv')
+
+
+def get_access_log_list(nginx_log_dir):
+	access_log_list = []
+	ip_pattern = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+	for nld_file in os.listdir(nginx_log_dir):
+		nld_file_path = os.path.join(nginx_log_dir, nld_file)
+
+		fsize = int(os.path.getsize(nld_file_path) / 1024)
+		if fsize < 1024:
+
+			with open(nld_file_path, 'r') as nld_file_obj:
+				nld_file_fl = nld_file_obj.readline()
+
+			if ((nld_file.endswith('.log')) and (ip_pattern.match(nld_file_fl.split(' ')[0]))):
+				access_log_list.append(nld_file_path)
+
+		else:
+			print(f"Ignoring big file: {nld_file_path}")
+
+	access_log_list.sort(key=os.path.getctime)
+	return access_log_list
+
+def get_short_country_name(uniq_ip):
+	short_country = ""
+	whois_cmd = [f"/usr/bin/whois {uniq_ip} | grep -i 'country' | head -1"]
+	process_whois = subprocess.Popen(whois_cmd, stdout=subprocess.PIPE, shell=True)
+	short_country = process_whois.communicate()[0].decode('utf-8').rsplit(' ', 1)[-1]
+	
+	if ( (re.search("^network:Country-Code:", short_country)) or 
+		(re.search("^network:Country:", short_country)) or 
+		(re.search("^contact:Country-Code:", short_country)) ):
+		short_country = short_country.split(":")[-1]
+	
+	process_whois.stdout.close()
+	
+	return str(short_country).rstrip("\n") if short_country else "NONE"
+
+
+def read_access_log_file(access_log_file):
+	ipaddress_list = []
+	user_agent = []
+	data_combain = {}
+
+	with open(access_log_file, 'r') as alf:
+		alf_data = alf.readlines()
+		ipaddress_list = set([ alfr.split(' ')[0] for alfr in alf_data ])
+
+		num_count = 0
+
+		for uil in ipaddress_list:
+
+			num_count +=1
+
+			try:
+			
+			    user_agent = [" ".join(alfr.split(' ')[11:-1]) for alfr in alf_data if uil == alfr.split(' ')[0]]
+			    request_url = [alfr.split(' ')[6] + ', ' for alfr in alf_data if uil == alfr.split(' ')[0]]
+			    short_country_name = get_short_country_name(uil).upper()
+			
+			except Exception as e:
+				print(e)
+
+			data_combain[uil] = {
+			    '#': num_count,
+			    'ipaddress': uil,
+			    'user_agent': " ".join((set(user_agent))) if " ".join((set(user_agent))) else "NONE",
+			    'total_user_agent': len(user_agent),
+			    'request_url': "".join((set(request_url))) if "".join((set(request_url))) else "NONE",
+			    'total_request_url': len(request_url),
+			    'client_country': short_country_name
+			    }
+
+	return data_combain
+
+def writerow_to_csv(access_log_file):
+	with open(RESULT_FILE, "a") as alf:
+		fieldnames = ['#', 'ipaddress', 'user_agent', 'total_user_agent', 'request_url', 'total_request_url', 'client_country']
+		alf_writer = csv.DictWriter(alf, delimiter=' ', fieldnames=fieldnames)
+		alf_writer.writeheader()
+
+		for ralfv in read_access_log_file(access_log_file).values():
+			alf_writer.writerow(ralfv)
+
+def main():
+
+
+	print(f"start: {datetime.datetime.now()}")
+	
+	for access_log_file in get_access_log_list(NGINX_LOG_DIR):
+		print(f"run for: {access_log_file}")
+		writerow_to_csv(access_log_file)
+
+	print(f"stop: {datetime.datetime.now()}")
+
+
+if __name__ == '__main__':
+	main()
